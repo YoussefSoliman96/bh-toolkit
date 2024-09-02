c1d6e9f8fff79ad5ad47bc1ac25e172906b82153
youssef soliman <youssef.soliman6636@gmail.com>
2024-09-02 11:25:16 +0300

    Set up Radix UI

--- app/providers/page.tsx
+++ app/providers/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@radix-ui/themes";
 import React from "react";
 
 const ProvidersPage = () => {
-  return <div>ProvidersPage</div>;
+  return (
+    <div>
+      <Button>New provider</Button>
+    </div>
+  );
 };
 
 export default ProvidersPage;
