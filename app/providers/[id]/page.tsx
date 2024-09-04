c995d5512f3c414e6d70d580b529c5ec1468a152
youssef soliman <youssef.soliman6636@gmail.com>
2024-09-04 15:15:27 +0300

    Add the edit button

--- app/providers/[id]/page.tsx
+++ app/providers/[id]/page.tsx
@@ -1,5 +1,7 @@
 import prisma from "@/prisma/client";
-import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import { Pencil2Icon } from "@radix-ui/react-icons";
+import { Box, Button, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 
 interface Props {
@@ -15,20 +17,28 @@ const ProviderDetailPage = async ({ params }: Props) => {
 
   if (!provider) notFound();
   return (
-    <div>
-      <Heading>
-        {provider.title} {provider?.firstName} {provider.lastName}
-      </Heading>
-      <Flex gap="9" my="2">
-        <Text>Evaluation: {provider.evaluation}</Text>
-        <Text>Follow up: {provider.followUp}</Text>
-      </Flex>
-      <Card mt="4" className="max-w-md">
-        <Text as="p">Languages: {provider.languages}</Text>
-        <Text as="p">Age range: {provider.ageRange}</Text>
-        <Text as="p">Working hours: {provider.workingHours}</Text>
-      </Card>
-    </div>
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Box>
+        <Heading>
+          {provider.title} {provider?.firstName} {provider.lastName}
+        </Heading>
+        <Flex gap="9" my="2">
+          <Text>Evaluation: {provider.evaluation}</Text>
+          <Text>Follow up: {provider.followUp}</Text>
+        </Flex>
+        <Card mt="4" className="max-w-md">
+          <Text as="p">Languages: {provider.languages}</Text>
+          <Text as="p">Age range: {provider.ageRange}</Text>
+          <Text as="p">Working hours: {provider.workingHours}</Text>
+        </Card>
+      </Box>
+      <Box>
+        <Button>
+          <Pencil2Icon />
+          <Link href={`/providers/${provider.id}/edit`}>Edit Provider</Link>
+        </Button>
+      </Box>
+    </Grid>
   );
 };
 
