e4efbb8899fe233e9f0ad4895533b85e4d88afd2
youssef soliman <youssef.soliman6636@gmail.com>
2024-09-04 14:54:56 +0300

    Add additional loading skeletons

--- app/providers/[id]/loading.tsx
+++ app/providers/[id]/loading.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { Heading, Flex, Card, Box, Text } from "@radix-ui/themes";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const LoadingProviderDetailPage = () => {
-  return <div>Loading...</div>;
+  return (
+    <Box maxWidth="xl">
+      <Skeleton />
+      <Flex gap="9" my="2">
+        <Skeleton width="5rem" />
+        <Skeleton width="2rem" />
+      </Flex>
+      <Card mt="4" className="max-w-md">
+        <Skeleton count={3} />
+      </Card>
+    </Box>
+  );
 };
 
 export default LoadingProviderDetailPage;
--- app/providers/new/loading.tsx
+++ app/providers/new/loading.tsx
@@ -1,7 +1,25 @@
+import { Box, Flex, Card } from "@radix-ui/themes";
 import React from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const LoadingNewProviderPage = () => {
-  return <div>Loading...</div>;
+  return (
+    <Box maxWidth="xl">
+      <Skeleton />
+      <Flex gap="9" my="2">
+        <Skeleton width="7rem" />
+        <Skeleton width="7rem" />
+      </Flex>
+      <Flex gap="9" my="2">
+        <Skeleton width="2rem" />
+        <Skeleton width="2rem" />
+      </Flex>
+      <Card mt="4" className="max-w-md">
+        <Skeleton count={5} />
+      </Card>
+    </Box>
+  );
 };
 
 export default LoadingNewProviderPage;
