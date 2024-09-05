9a1859a2c17255d83fa6692316acb8cbaefa6505
youssef soliman <youssef.soliman6636@gmail.com>
2024-09-05 19:56:38 +0300

    Fix the role filter

--- app/providers/list/ProviderFilter.tsx
+++ app/providers/list/ProviderFilter.tsx
@@ -1,21 +1,28 @@
 "use client";
 import { Role } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
+import { useRouter } from "next/navigation";
 
-const roles: { label: string; value: Role }[] = [
-  { label: "All", value: "PSYCHIATRIST" },
+const roles: { label: string; value?: Role }[] = [
+  { label: "All" },
   { label: "Psychiatrist", value: "PSYCHIATRIST" },
   { label: "Therapist", value: "THERAPIST" },
   { label: "Residency", value: "RESIDENCY" },
 ];
 
 const ProviderFilter = () => {
+  const router = useRouter();
   return (
-    <Select.Root>
+    <Select.Root
+      onValueChange={(role) => {
+        const query = role === "ALL" ? " " : `?role=${role}`;
+        router.push("/providers/list" + query);
+      }}
+    >
       <Select.Trigger placeholder="Filter by role" />
       <Select.Content>
         {roles.map((role) => (
-          <Select.Item key={role.label} value={role.value}>
+          <Select.Item key={role.label} value={role.value ?? "ALL"}>
             {role.label}
           </Select.Item>
         ))}
