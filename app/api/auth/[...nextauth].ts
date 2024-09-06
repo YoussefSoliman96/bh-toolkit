951716dce0e638abb3d3e7becf1ab9d913e1de21
youssef soliman <youssef.soliman6636@gmail.com>
2024-09-06 19:25:20 +0300

    Set up NextAuth

---
+++ app/api/auth/[...nextauth].ts
@@ -0,0 +1,33 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "username" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const { username, password } = credentials as {
+          username: string;
+          password: string;
+        };
+        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
+
+        if (user) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
