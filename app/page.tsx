96c56bbfdabda3969b8f920f9b2e7fe95a84f007
youssef soliman <youssef.soliman6636@gmail.com>
2024-09-13 03:46:26 +0300

    Add reminders to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 import { Metadata } from "next";
+import Reminders from "./reminders/Reminders";
+import AddReminderButton from "./reminders/AddReminderButton";
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <AddReminderButton />
+      <Reminders />
+    </>
+  );
 }
 
 export const metadata: Metadata = {
